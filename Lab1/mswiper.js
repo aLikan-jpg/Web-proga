4818ca693f613b39d434000f0e317dfb454f6e27
aLikan-jpg <71884616+aLikan-jpg@users.noreply.github.com>
2021-12-24 10:39:05 +0300

    labs

---
+++ Lab1/mswiper.js
@@ -0,0 +1,11 @@
+new Swiper('.image-slider',{
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    speed: 600,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+});
--- Lab1/script.js
+++ Lab1/script.js
@@ -4,3 +4,10 @@
         document.getElementById('load-time').innerHTML=((new Date).getTime() - window.startTime) + 'ms';
     });
 })();
+window.onload= function (){
+    document.querySelectorAll("[data-nav]").forEach(function (element){
+        if(element.href === document.location.href){
+            element.classList.add("nav");
+        }
+    })
+}
